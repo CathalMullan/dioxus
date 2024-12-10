ec4ee7d19535c70b6c964e8f58c425425ebde2a1
Cathal Mullan <contact@cathal.dev>
2024-12-10 19:36:09 +0000

    Nixify v0.6.0

--- flake.nix
+++ flake.nix
@@ -1,69 +1,171 @@
 {
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    systems.url = "github:nix-systems/default";
+  description = "dioxus";
 
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    # crane.url = "github:ipetkov/crane";
-    # crane.inputs.nixpkgs.follows = "nixpkgs";
+  inputs = {
+    nixpkgs = {
+      url = "github:CathalMullan/nixpkgs/dioxus-cli-v0.6.0";
+    };
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+
+    android-nixpkgs = {
+      url = "github:tadfisher/android-nixpkgs";
+
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
   };
 
-  outputs = inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import inputs.systems;
+  # nix flake show
+  outputs =
+    {
+      nixpkgs,
+      rust-overlay,
+      android-nixpkgs,
+      ...
+    }:
 
-      perSystem = { config, self', pkgs, lib, system, ... }:
-        let
-          rustToolchain = pkgs.rust-bin.stable.latest.default.override {
-            extensions = [
-              "rust-src"
-              "rust-analyzer"
-              "clippy"
-            ];
-          };
-          rustBuildInputs = [
-            pkgs.openssl
-            pkgs.libiconv
-            pkgs.pkg-config
-          ] ++ lib.optionals pkgs.stdenv.isLinux [
-            pkgs.glib
-            pkgs.gtk3
-            pkgs.libsoup_3
-            pkgs.webkitgtk_4_1
-            pkgs.xdotool
-          ] ++ lib.optionals pkgs.stdenv.isDarwin (with pkgs.darwin.apple_sdk.frameworks; [
-            IOKit
-            Carbon
-            WebKit
-            Security
-            Cocoa
-          ]);
+    let
+      perSystem = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
 
-          # This is useful when building crates as packages
-          # Note that it does require a `Cargo.lock` which this repo does not have
-          # craneLib = (inputs.crane.mkLib pkgs).overrideToolchain rustToolchain;
-        in
-        {
-          _module.args.pkgs = import inputs.nixpkgs {
-            inherit system;
-            overlays = [
-              inputs.rust-overlay.overlays.default
-            ];
+      systemPkgs = perSystem (
+        system:
+
+        import nixpkgs {
+          inherit system;
+
+          config = {
+            allowUnfreePredicate =
+              pkg:
+              builtins.elem (nixpkgs.lib.getName pkg) [
+                "Xcode.app"
+              ];
           };
 
-          devShells.default = pkgs.mkShell {
-            name = "dioxus-dev";
-            buildInputs = rustBuildInputs;
-            nativeBuildInputs = [
-              # Add shell dependencies here
-              rustToolchain
-            ];
-            shellHook = ''
-              # For rust-analyzer 'hover' tooltips to work.
-              export RUST_SRC_PATH="${rustToolchain}/lib/rustlib/src/rust/library";
-            '';
+          overlays = [
+            (import rust-overlay)
+
+            (final: prev: {
+              rust-toolchain = prev.rust-bin.stable.latest.default.override {
+                targets = [
+                  # Desktop
+                  "aarch64-apple-darwin"
+                  # Web
+                  "wasm32-unknown-unknown"
+                  # Android
+                  "aarch64-linux-android"
+                  "armv7-linux-androideabi"
+                  "i686-linux-android"
+                  "x86_64-linux-android"
+                  # iOS
+                  "aarch64-apple-ios-sim"
+                  "aarch64-apple-ios"
+                  "x86_64-apple-ios"
+                ];
+
+                extensions = [
+                  "clippy"
+                  "rust-analyzer"
+                  "rust-docs"
+                  "rust-src"
+                  "rustfmt"
+                ];
+              };
+
+              android-sdk = android-nixpkgs.sdk.${system} (
+                sdkPkgs: with sdkPkgs; [
+                  platform-tools
+                  cmdline-tools-latest
+                  emulator
+                  ndk-25-2-9519653
+                  platforms-android-33
+                  platforms-android-34
+                  build-tools-34-0-0
+                  system-images-android-34-google-apis-arm64-v8a
+                ]
+              );
+            })
+          ];
+        }
+      );
+
+      perSystemPkgs = f: perSystem (system: f (systemPkgs.${system}));
+    in
+    {
+      devShells = perSystemPkgs (pkgs: {
+        # nix develop
+        default = pkgs.mkShellNoCC {
+          name = "dioxus-shell";
+
+          env = {
+            NIX_PATH = "nixpkgs=${nixpkgs.outPath}";
+            OPENSSL_NO_VENDOR = "1";
+
+            # iOS
+            CARGO_TARGET_AARCH64_APPLE_IOS_SIM_RUSTFLAGS = "-C linker=gcc";
           };
+
+          shellHook = ''
+            # iOS
+            export SDKROOT="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk"
+            export DEVELOPER_DIR="/Applications/Xcode.app/Contents/Developer"
+
+            # Android
+            if [ ! -d "$HOME/.android/avd/Dioxus.avd" ]; then
+              echo "Creating Android Virtual Device in '$HOME/.android/avd'"
+              avdmanager create avd \
+                --name "Dioxus" \
+                --package "system-images;android-34;google_apis;arm64-v8a" \
+                --device "pixel_5"
+            fi
+
+            echo ""
+            echo "Desktop:"
+            echo "  1. dx serve --example hello_world --platform desktop"
+            echo ""
+            echo "Web:"
+            echo "  1. dx serve --example hello_world --platform web"
+            echo ""
+            echo "Android:"
+            echo "  1. emulator -avd Dioxus"
+            echo "  2. dx serve --example hello_world --platform android"
+            echo ""
+            echo "iOS:"
+            echo "  1. xcrun simctl boot 'iPhone 15'"
+            echo "  2. open -a Simulator"
+            echo "  3. dx serve --example hello_world --platform ios"
+            echo ""
+          '';
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            openssl
+          ];
+
+          buildInputs = with pkgs; [
+            # Dioxus
+            dioxus-cli
+
+            # Rust
+            rust-toolchain
+
+            # Android
+            android-sdk
+            jdk
+
+            # Nix
+            nixfmt-rfc-style
+            nixd
+            nil
+          ];
         };
+      });
     };
 }
